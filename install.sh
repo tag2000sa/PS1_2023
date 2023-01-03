55da5de7e4f6f62ae79cdd390f41851b63841638
TurkyGary_uClab <tsaderaldin@kau.edu.sa>
2023-01-03 08:00:44 +0000

    generalize installation process

--- install.sh
+++ install.sh
@@ -1,11 +1,56 @@
 #!/bin/bash
 #Author: Turky Abdulaziz Saderaldin
 #Create Date: 01/01/23 23:02:28
-#Update Date: 02/01/23 19:40:42
+#Update Date: 03/01/23 07:55:00
 #File Name: install.sh
 #Subject: Installation Script
 
 sudo echo ''
+
+# Error Messages
+err1='more than one input.'
+err2="invalid input.\nvalid values are {JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC}"
+err3="file (${dd}) not found."
+
+# function test input exist in a list
+isIn() { [[ $2 =~ (^|[[:space:]])$1($|[[:space:]]) ]] }
+
+# set default design input
+input=`date +%b` && input=${input^^}
+
+# check input
+if [[ $# -eq 1 ]]; then
+        isIn $1 "JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC"
+        if [[ $? -eq 0 ]]; then
+                input=$1
+        else
+                echo -e "Error: ${err2}" && exit 1
+        fi
+elif [[ $# -gt 1 ]]; then
+        echo -e "Error: ${err1}" && exit 1
+fi
+
+# find correspond contiguration file
+case ${input} in
+        "JAN") dd=PS1_Jan2023.sh;;
+        "FEB") dd=PS1_Feb2023.sh;;
+        "MAR") dd=PS1_Mar2023.sh;;
+        "APR") dd=PS1_Apr2023.sh;;
+        "MAY") dd=PS1_May2023.sh;;
+        "JUN") dd=PS1_Jun2023.sh;;
+        "JUL") dd=PS1_Jul2023.sh;;
+        "AUG") dd=PS1_Aug2023.sh;;
+        "SEP") dd=PS1_Sep2023.sh;;
+        "OCT") dd=PS1_Oct2023.sh;;
+        "NOV") dd=PS1_Nov2023.sh;;
+        "DEC") dd=PS1_Dec2023.sh;;
+esac
+
+# check design file existance
+if [[ ! -e ./${dd} ]]; then
+    echo -e " Error: ${err3}" && exit 1
+fi
+
 # start installation message
 echo -e "Install tools ...\c"
 
@@ -66,14 +111,14 @@ fi
 
 # start script installation message
 echo 'Install script ...'
-# run script (PS1_Jan2023.sh) as normal user
+# run script as normal user
 echo -e " - install script as normal user\c"
-./PS1_Jan2023.sh
+./${dd}
 echo -e "\tdone"
 
-# re-run script (PS1_Jan2023.sh) as root user
+# re-run script as root user
 echo -e " - install script as root user\c"
-sudo ./PS1_Jan2023.sh
+sudo ./${dd}
 echo -e "\t\tdone"
 
 # complete installation message
